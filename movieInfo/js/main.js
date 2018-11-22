643834302e075c9bee16c072de9f6f65b1b45045
nyKrisz <ny.krisz04@gmail.com>
2018-11-22 19:36:19 +0100

    movie page added & selected movie info

--- movieInfo/js/main.js
+++ movieInfo/js/main.js
@@ -1,5 +1,7 @@
-let formInput = document.getElementById('searchForm');
-formInput.addEventListener('submit', getMovies);
+window.onload = function(){
+    let formInput = document.getElementById('searchForm');
+    formInput.addEventListener('submit', getMovies);
+}
 
 function getMovies(e){
     //form input text
@@ -28,8 +30,55 @@ function getMovies(e){
     e.preventDefault();
 }
 
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
 
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
 
+    axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
+        .then((response) => {
+            let movie = response.data;
+
+            let output = `
+                <div class="row">
+                    <div class="col-md-4 m-2">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    
+                    <div class="col-md-7">
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="bd list-group-item bg-dark"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class="bd list-group-item bg-dark"><strong>Released:</strong> ${movie.Released}</li>
+                            <li class="bd list-group-item bg-dark"><strong>Rated:</strong> ${movie.Rated}</li>
+                            <li class="bd list-group-item bg-dark"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                            <li class="bd list-group-item bg-dark"><strong>Director:</strong> ${movie.Director}</li>
+                            <li class="bd list-group-item bg-dark"><strong>Writer:</strong> ${movie.Writer}</li>
+                            <li class="bd list-group-item bg-dark"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mt-2 mb-2">
+                    <div class="col-md-12 text-center">
+                        <h3>Plot</h3>
+                        <p>${movie.Plot}</p>
+                        <hr>
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View Imdb</a>
+                        <a href="index.html" class="btn btn-light">Go Back To Search</a>
+                    </div>
+                </div>
+            `;
+
+            document.getElementById('movie').innerHTML = output;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
 
 
 
